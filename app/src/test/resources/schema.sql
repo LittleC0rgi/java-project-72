82e8af79b581f8e52110fbbf9bfff1ac379a0141
Kirill Vasilev <kirillwork56@gmail.com>
2026-07-07 10:45:03 +0300

    Fix tests

--- app/src/test/resources/schema.sql
+++ app/src/test/resources/schema.sql
@@ -3,5 +3,18 @@ DROP TABLE IF EXISTS urls;
 CREATE TABLE urls (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    status_code INT,
+    title VARCHAR(255),
+    h1 VARCHAR(255),
+    description TEXT,
+    url_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (url_id) REFERENCES urls(id)
+)
